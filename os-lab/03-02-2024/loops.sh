9993a767f3afb3b96c54e01c1ddf80fb09b347ed
Aman Pathak <chaos69480@gmail.com>
2024-02-03 14:57:28 +0530

    array indexing in loops

--- os-lab/03-02-2024/loops.sh
+++ os-lab/03-02-2024/loops.sh
@@ -4,13 +4,8 @@
 #
 arr=(1 2 3 "ABC" 4.5 "X")
 
-for item in arr
+for item in ${arr[*]}
 do
     echo $item
 done
-
-echo ${#arr[*]} #length of entire array
-echo ${#arr[1]} #length of first element
-echo ${arr[1]} #first element
-
 
